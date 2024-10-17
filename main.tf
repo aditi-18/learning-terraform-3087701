3d1f8af7d0877c738d3e95b73d2f8ed80bdb4e0e
aditi-18 <42387786+aditi-18@users.noreply.github.com>
2024-10-17 11:43:41 +1100

    sol fixing

--- main.tf
+++ main.tf
@@ -43,16 +43,10 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-  
-  image_id           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  
-}
-
-resource "aws_autoscaling_attachment" "asg_target_group_attachment" {
-  autoscaling_group_name = module.autoscaling.autoscaling_group_name
-  target_group_arn       = module.blog_alb.target_groups["ex-instance"].arn
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
